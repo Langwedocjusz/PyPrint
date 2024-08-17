e6d054916860c12b464c23517de00b8afae76a6b
Langwedocjusz <mikolaj.pedro@gmail.com>
2024-08-17 10:58:38 +0200

    Enabled more warnings, added printing iterables

--- include/PyPrint.h
+++ include/PyPrint.h
@@ -4,16 +4,47 @@
 
 namespace pp
 {
-
 template <typename T>
 concept Printable = requires(T val) { std::cout << val; };
+
+template <typename T>
+concept NonPrintable = !Printable<T>;
+
+template <typename T>
+concept Iterable = requires(T val) {
+    val.begin();
+    val.end();
+};
+
+template <typename T>
+concept NonPrintableIterable = NonPrintable<T> && Iterable<T>;
 
 template <Printable P> void print(const P &p)
 {
     std::cout << p;
 }
 
-template <Printable P> void println(const P &p)
+template <NonPrintableIterable T> void print(const T &val)
+{
+    std::cout << "[";
+
+    auto it = val.begin();
+
+    if (it != val.end())
+    {
+        print(*it);
+
+        while (++it != val.end())
+        {
+            std::cout << ", ";
+            print(*it);
+        }
+    }
+
+    std::cout << "]";
+}
+
+template <typename T> void println(const T &p)
 {
     print(p);
     std::cout << '\n';
--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -2,13 +2,11 @@
 
 #include "PyPrint.h"
 
+#include <array>
+
 TEST_CASE("String literal")
 {
-    TestCoutFn(
-        []() {
-            pp::println("String literal!");
-        },
-        "String literal!\n");
+    TestCoutFn([]() { pp::println("String literal!"); }, "String literal!\n");
 }
 
 TEST_CASE("std::string")
@@ -31,4 +29,42 @@ TEST_CASE("std::string_view")
             pp::println(sv);
         },
         "A std::string_view\n");
+}
+
+TEST_CASE("Printing numbers")
+{
+    TestCoutFn([]() { pp::println(4); }, "4\n");
+
+    TestCoutFn([]() { pp::println(139.48f); }, "139.48\n");
+}
+
+TEST_CASE("Printing array-likes")
+{
+    TestCoutFn(
+        []() {
+            std::array<int, 4> arr{0, 1, 2, 3};
+            pp::println(arr);
+        },
+        "[0, 1, 2, 3]\n");
+
+    TestCoutFn(
+        []() {
+            std::vector<float> arr{0.5f, 7.8f, 12.4f, 159.6f, 242.1f};
+            pp::println(arr);
+        },
+        "[0.5, 7.8, 12.4, 159.6, 242.1]\n");
+
+    TestCoutFn(
+        []() {
+            std::array<std::array<unsigned int, 2>, 2> arr{{{{0, 1}}, {{2, 3}}}};
+            pp::println(arr);
+        },
+        "[[0, 1], [2, 3]]\n");
+
+    TestCoutFn(
+        []() {
+            std::vector<std::vector<double>> arr{{0.0f, 1.0f, 2.0f}, {3.0f, 4.0f, 5.0f}, {6.0f, 7.0f, 8.0f}};
+            pp::println(arr);
+        },
+        "[[0, 1, 2], [3, 4, 5], [6, 7, 8]]\n");
 }
